2372192fdcbd7ad32a83e8c5303361e6889ced13
JustinHinton <justgawd7@icloud.com>
2024-06-03 20:29:28 -0600

    added notes.js

---
+++ routes/notes.js
@@ -0,0 +1,6 @@
+// added middleware for the notes
+const notes = require("express").Router();
+const path = require("path");
+notes.get('/', (req, res) => res.sendFile(path.join(__dirname, '/notes')));
+
+module.exports = notes;
