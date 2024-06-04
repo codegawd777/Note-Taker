e7d5676a843183f94dc1a58f8009827b5e24f06d
JustinHinton <justgawd7@icloud.com>
2024-06-03 20:30:26 -0600

    updated file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const path = require ('path');
-const { clog } = require(`./middleware/clog`);
-const api = require(`./routes/index.js`);
+// const { clog } = require(`./middleware/clog`);
+const api = require('./routes/api.js');
+const notes = require('./routes/notes.js');
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -9,22 +11,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // import custom middleware
-app.use(clog);
+// app.use(clog);
 
+app.use(express.static('public', { extensions: ['html', 'htm'] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', api);
+app.use('/notes', notes);
 
-app.use(express.static('public'));
-
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-
-
-app.get('/feedback', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('*', (req, res) => 
+res.sendFile(path.join(__dirname, '/public')));
 
 
 app.listen(PORT, () => {
